8f6821d85771f3b0c20a95c061a409e43245cfa3
Salted fish with dreams <70441023+yechuze@users.noreply.github.com>
2021-10-01 00:07:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-import datetime
+import time
 with open('time_log.txt', 'w', encoding='utf-8') as f:
   f.write('test')
-local_time_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print(local_time_str)
+print(time.time())
