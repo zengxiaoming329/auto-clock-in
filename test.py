ab4ed7ef98f12985790c30d72503dc376b75a07e
Salted fish with dreams <70441023+yechuze@users.noreply.github.com>
2021-09-30 22:33:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ import datetime
 with open('time_log.txt', 'w', encoding='utf-8') as f:
   f.write('test')
 local_time_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-print(kr_time_str)
+print(local_time_str)
